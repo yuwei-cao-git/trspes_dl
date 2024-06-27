d4f9d3064afd4587039d845f2eb01df2baff5223
yuwei-cao-git <yuwei.cao94@gmail.com>
2024-06-26 22:54:22 -0700

    'gnn device'

--- Pytorch/models/PointAugment/utils/train_ddp.py
+++ Pytorch/models/PointAugment/utils/train_ddp.py
@@ -152,11 +152,6 @@ def train(params, io, trainset, testset):
     test_sampler = DistributedSampler(dataset=testset)
     train_loader = DataLoader(trainset, batch_size=params["batch_size"], shuffle=(train_sampler is None), num_workers=num_workers, pin_memory=True, sampler=train_sampler)
     test_loader = DataLoader(testset, batch_size=params["batch_size"], shuffle=False, num_workers=num_workers, pin_memory=True, sampler=test_sampler)
-    # store loss on local rank
-    #criterion_aug = loss_utils.g_loss(weights).cuda()
-    #criterion_aug_calc = loss_utils.d_loss(weights).cuda()
-    #criterion_calc = loss_utils.calc_loss(weights).cuda()
-    #criterion_mse = F.mse_loss().cuda()
 
     # Iterate through number of epochs
     for epoch in tqdm(range(params["epochs"]), desc="Model Total: ", leave=False, colour="red"):
