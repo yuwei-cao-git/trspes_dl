a22653e77d6dc8fe75dc9b833725a5942278a342
yuwei-cao-git <yuwei.cao94@gmail.com>
2024-07-01 15:53:18 -0700

    'augmentor device setting'

--- Pytorch/models/PointAugment/augment/augmentor.py
+++ Pytorch/models/PointAugment/augment/augmentor.py
@@ -12,9 +12,10 @@ from torch.autograd import Variable
 
 def batch_quat_to_rotmat(q, out=None):
     B = q.size(0)
+    device = q.device
 
     if out is None:
-        out = q.new_empty(B, 3, 3)
+        out = q.new_empty(B, 3, 3).to(device)
 
     # 2 / squared quaternion 2-norm
     leng = torch.sum(q.pow(2), 1)
@@ -54,8 +55,8 @@ class AugmentorRotation(nn.Module):
         x = F.relu(self.bn1(self.fc1(x)))
         x = F.relu(self.bn2(self.fc2(x)))
         x = self.fc3(x)
-
-        iden = x.new_tensor([1, 0, 0, 0])
+        device=x.device
+        iden = x.new_tensor([1, 0, 0, 0]).to(device)
         x = x + iden
 
         x, s = batch_quat_to_rotmat(x)
@@ -109,7 +110,7 @@ class Augmentor(nn.Module):
         B, C, N = data.size() # batch, channels, num points
         raw_pt = data[:, :3, :].contiguous()
         normal = data[:, 3:, :].transpose(1, 2).contiguous() if C > 3 else None
-        
+        device = data.device
         x = F.relu(self.bn1(self.conv1(raw_pt)))
         x = F.relu(self.bn2(self.conv2(x)))
         pointfeat = x
@@ -128,14 +129,14 @@ class Augmentor(nn.Module):
 
         pt = raw_pt.transpose(2, 1).contiguous()
 
-        p1 = random.uniform(0, 1)
+        p1 = random.uniform(0, 1).to(device)
         possi = 0.0
         if p1 > possi:
             pt = torch.bmm(pt, rotation).transpose(1, 2).contiguous()
         else:
             pt = pt.transpose(1, 2).contiguous()
 
-        p2 = random.uniform(0, 1)
+        p2 = random.uniform(0, 1).to(device)
         if p2 > possi:
             pt = pt + displacement
 
